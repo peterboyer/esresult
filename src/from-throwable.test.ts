5ba03111ef0cbeda650f01fd9e4f3fe9cad774a6
Peter Boyer <8391902+ptboyer@users.noreply.github.com>
2021-12-06 19:54:03 +1100

    fix: fromThrowable to support async/promise return-types

--- src/from-throwable.test.ts
+++ src/from-throwable.test.ts
@@ -28,3 +28,25 @@ test("with invalid input throw 'invalid'", () => {
   const $ = safeParse("@invalidinput");
   expect(!$.ok && $.error).toMatchObject(new TypeError("Invalid source."));
 });
+
+async function parseAsync(source: unknown): Promise<string> {
+  if (typeof source !== "string") throw new TypeError("Bad source.");
+  if (source.includes("@")) throw new TypeError("Invalid source.");
+  return source;
+}
+
+const safeParseAsync = fromThrowable(parseAsync);
+
+expectType<Promise<Result<string>>>(safeParseAsync(true));
+
+describe("async", () => {
+  test("with valid input", async () => {
+    const $ = await safeParseAsync("validasyncinput");
+    expect($.ok && $.value).toBe("validasyncinput");
+  });
+
+  test("with invalid input throw 'bad'", async () => {
+    const $ = await safeParseAsync(false);
+    expect(!$.ok && $.error).toMatchObject(new TypeError("Bad source."));
+  });
+});
--- src/from-throwable.ts
+++ src/from-throwable.ts
@@ -2,12 +2,20 @@ import { ok } from "./ok";
 import { err } from "./err";
 import { Result } from "./result";
 
+// https://stackoverflow.com/a/61626123
+type IfAny<T, Y, N> = 0 extends 1 & T ? Y : N;
+type IsAny<T> = IfAny<T, true, false>;
+
+// TODO: Remove in favour of built-in utility type on TypeScript 4.5 release.
+type Awaited<T> = T extends Promise<infer Result> ? Result : never;
+
 /**
  * Wraps a throwable function so that it cannot throw and instead returns:
  * - `Ok` with its normal return value, or
  * - `Err` with any thrown error value.
  *
- * @param fn Throwable function to be wrapped.
+ * @param fn Throwable function to be wrapped. Also supports async functions/
+ * Promise-based return values.
  *
  * @example
  * ```
@@ -23,10 +31,32 @@ import { Result } from "./result";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function fromThrowable<FN extends (...args: any[]) => any>(fn: FN) {
-  function wrappedFn(...args: Parameters<FN>): Result<ReturnType<FN>, unknown> {
+  function wrappedFn(
+    ...args: Parameters<FN>
+  ): // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  IsAny<ReturnType<FN>> extends true
+    ? Result<ReturnType<FN>>
+    : // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    ReturnType<FN> extends Promise<any>
+    ? Promise<Result<Awaited<ReturnType<FN>>>>
+    : Result<ReturnType<FN>> {
     try {
-      return ok(fn(...args));
+      const valueOrPromise = fn(...args);
+
+      // if Promise/thenable
+      if (!!valueOrPromise && typeof valueOrPromise.then === "function") {
+        // @ts-expect-error TODO: Figure out how to gracefully handle this.
+        return valueOrPromise
+          .then((value: unknown) => ok(value))
+          .catch((e: unknown) => err.primitive(e)) as Promise<
+          Result<Awaited<ReturnType<FN>>>
+        >;
+      }
+
+      // @ts-expect-error TODO: Figure out how to gracefully handle this.
+      return ok(valueOrPromise);
     } catch (e) {
+      // @ts-expect-error TODO: Figure out how to gracefully handle this.
       return err.primitive(e);
     }
   }
