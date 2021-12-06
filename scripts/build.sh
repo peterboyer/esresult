57bac88d9fdd0bc4bb0e43461c482de990ab061e
Peter Boyer <8391902+ptboyer@users.noreply.github.com>
2021-12-06 22:20:13 +1100

    build: include README.md in dist

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,6 @@
 
 rm -rf dist \
   && tsc --project tsconfig.build.json \
-  && cp package.json yarn.lock dist \
+  && cp package.json yarn.lock README.md dist \
   && dot-json dist/package.json scripts --delete \
   && dot-json dist/package.json devDependencies --delete
