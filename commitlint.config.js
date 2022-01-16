c6390e67ac2a872584757864a45444244a1099f6
Peter Boyer <8391902+ptboyer@users.noreply.github.com>
2022-01-17 07:08:37 +1100

    dev: commitlint config update

---
+++ commitlint.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "scope-enum": [2, "never"],
+    "body-case": [2, "always", "lower-case"],
+    "scope-case": [2, "always", "kebab-case"],
+    "header-case": [2, "always", "lower-case"],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "fix", // bug fix, bumps PATCH
+        "feat", // new feature, bumps MINOR
+        "test", // adding/refactoring tests only
+        "refactor", // code changes, without new fixes or features
+        "style", // formatting changes, i.e. semicolons
+        "docs", // updates to docs/comments
+        "ci", // updates to ci/workflow config
+        "build", // updates to build config
+        "revert", // revert changes to unblock failing ci/tests
+        "wip", // work-in-progress code commits as part of bigger features
+        "dev", // general changes to developer-environment/internal tooling
+        "chore", // bumping dependencies and other general maintenance
+      ],
+    ],
+  },
+};
