ad6cc06b8dc45765d3309d9d1f4ea25b8bcf0520
Peter Boyer <8391902+ptboyer@users.noreply.github.com>
2022-05-02 20:00:58 +1000

    fix: missing re-exports from global.ts

--- src/global.ts
+++ src/global.ts
@@ -10,12 +10,15 @@ declare global {
 
   namespace Result {
     type Any = _Result.Any;
+    type OrThrown<V = void> = _Result.OrThrown<V>;
     type Async<V = void, E = never> = _Result.Async<V, E>;
     type AsyncAny = _Result.AsyncAny;
-    type Value<V> = _Result.Error<V>;
+    type AsyncOrThrown<V = void> = _Result.AsyncOrThrown<V>;
+    type Value<V> = _Result.Value<V>;
     type ValueAny = _Result.ValueAny;
     type Error<E, V = never> = _Result.Error<E, V>;
     type ErrorAny = _Result.ErrorAny;
+    type ErrorThrown<V = never> = _Result.ErrorThrown<V>;
   }
 
   const Result: typeof _Result;
